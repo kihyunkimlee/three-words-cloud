d2b095196d34511e9767d0512ebd42c04ee7d548
kihyunkimlee <khkh4631@gmail.com>
2021-05-14 18:55:00 +0900

    Use babel-loader plugin to transcompile javascript files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,24 +1,34 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
-  mode: "development",
+  mode: 'production',
   entry: {
-    main: "./js/app.js",
+    main: './js/app.js',
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve("./dist"),
+    path: path.resolve('./dist'),
+    filename: 'main.js',
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, '/js')
+        ],
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/polyfill']
+          }
+        }
+      }
+    ]
   },
   devServer: {
     overlay: true,
-    stats: "errors-only",
+    stats: 'errors-only',
   },
 }
